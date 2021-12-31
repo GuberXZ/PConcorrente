9596b37298e7e6976b90f8d4ccd33a4d274813fe
David Fontoura <45210452+DavidFontoura@users.noreply.github.com>
2021-12-31 18:01:52 +0000

    Checks if output directories exist, creates if not Created function outpath() to return output file's path as string Checks if output file exists (by its path), ignores if yes Saves output file in appropriate directory

--- Project/ap-paralelo-complexo.py
+++ Project/ap-paralelo-complexo.py
@@ -1,32 +1,45 @@
 from PIL import Image as i
 import numpy as np
 from threading import Thread as thr
+from os.path import exists
+from os import mkdir
 
 path = 'small'
 t_number = 4
 
 new_width = 640
 size = 200
-wt = i.open('watermark.png')
+watermark = i.open('watermark.png')
+
+
+def outpath(im, subfolder):
+    directory, filename = im.filename.split('/')
+    return f'{directory}/{subfolder}/{filename}'
 
 
 def resize(image_list, new_width):
     for im in image_list:
+        if exists(new_path := outpath(im, 'Resized')): continue
         new_size = (new_width, int(im.size[1] * new_width / im.size[0]))
         new_im = im.resize(new_size)
+        new_im.save(new_path)
 
 
 def thumb(image_list, size):
     for im in image_list:
+        if exists(new_path := outpath(im, 'Thumbnails')): continue
         w, h = im.size
         new_size = (size, int(h * size / w)) if h > w else (int(w * size / h), size)
         new_im = im.resize(new_size).crop((0, 0, size, size))
+        new_im.save(new_path)
 
 
-def water(image_list, wt):
+def water(image_list, watermark):
     for im in image_list:
-        nem_im = im.copy()
-        nem_im.paste(wt, mask=wt)
+        if exists(new_path := outpath(im, 'Watermarks')): continue
+        new_im = im.copy()
+        new_im.paste(watermark, mask=watermark)
+        new_im.save(new_path)
 
 
 def main():
@@ -34,6 +47,10 @@ def main():
     with open(f'{path}/img-process-list.txt') as ipl:
         all_images = [i.open(f'{path}/{im.strip()}') for im in ipl]
     grouped_images = np.array_split(all_images, t_number)
+
+    not exists(dir := f'{path}/Resized') and mkdir(dir)
+    not exists(dir := f'{path}/Thumbnails') and mkdir(dir)
+    not exists(dir := f'{path}/Watermarks') and mkdir(dir)
 
     # Parallel #
     # Resizing
@@ -49,7 +66,7 @@ def main():
     (th.join() for th in thread_list)
 
     # water
-    thread_list = [thr(target=water, args=(images, wt)) for images in grouped_images]
+    thread_list = [thr(target=water, args=(images, watermark)) for images in grouped_images]
     for th in thread_list:
         th.start()
     (th.join() for th in thread_list)
--- Project/ap-paralelo-simples.py
+++ Project/ap-paralelo-simples.py
@@ -1,36 +1,46 @@
 from PIL import Image as i
 import numpy as np
 from threading import Thread as thr
+from os.path import exists
+from os import mkdir
 
 path = 'small'
 t_number = 4
 
 new_width = 640
 size = 200
-wt = i.open('watermark.png')
+watermark = i.open('watermark.png')
 
 
-def resize(im, new_width):
+def outpath(im, subfolder):
+    directory, filename = im.filename.split('/')
+    return f'{directory}/{subfolder}/{filename}'
+
+
+def resize(im, new_path, new_width):
     new_size = (new_width, int(im.size[1] * new_width / im.size[0]))
     new_im = im.resize(new_size)
+    new_im.save(new_path)
 
 
-def thumb(im, size):
+def thumb(im, new_path, size):
     w, h = im.size
     new_size = (size, int(h * size / w)) if h > w else (int(w * size / h), size)
     new_im = im.resize(new_size).crop((0, 0, size, size))
+    new_im.save(new_path)
 
 
-def water(im, wt):
-    nem_im = im.copy()
-    nem_im.paste(wt, mask=wt)
+def water(im, new_path, watermark):
+    new_im = im.copy()
+    new_im.paste(watermark, mask=watermark)
+    new_im.save(new_path)
 
 
-def full_transform(image_list, new_width, size, bwt):
+def full_transform(image_list, new_width, size, watermark):
     for im in image_list:
-        resize(im, new_width)
-        thumb(im, size)
-        water(im, wt)
+        if not exists(new_path := outpath(im, 'Resized')): resize(im, new_path, new_width)
+        if not exists(new_path := outpath(im, 'Thumbnails')): thumb(im, new_path, size)
+        if not exists(new_path := outpath(im, 'Watermarks')): water(im, new_path, watermark)
 
 
 def main():
@@ -39,8 +49,12 @@ def main():
         all_images = [i.open(f'{path}/{im.strip()}') for im in ipl]
     grouped_images = np.array_split(all_images, t_number)
 
+    not exists(dir := f'{path}/Resized') and mkdir(dir)
+    not exists(dir := f'{path}/Thumbnails') and mkdir(dir)
+    not exists(dir := f'{path}/Watermarks') and mkdir(dir)
+
     # Parallel #
-    thread_list = [thr(target=full_transform, args=(images, new_width, size, wt)) for images in grouped_images]
+    thread_list = [thr(target=full_transform, args=(images, new_width, size, watermark)) for images in grouped_images]
     for th in thread_list:
         th.start()
     (th.join() for th in thread_list)
